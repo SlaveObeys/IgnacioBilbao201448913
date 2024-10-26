20366e6efc33e4d2b6e9c1f95999a86da452f2c6
BuildTools <unconfigured@null.spigotmc.org>
2024-10-26 11:09:48 -0300

    Se crea diseño de los posts en el inicio, tipado fuerte y localstorage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,19 +16,23 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
 
-    //Obtenemos los datos de la API de POSTS
-    this.socialPostApi.obtenerPosts().subscribe((post => {
-      console.log("API: ", post);
-      // Si se encuentran posts en la API
-      if (Object.keys(post).length > 1) {
-        this.posts = post;
-      }
-      // Si está vacía la petición mostrar un mensaje de error
-      else {
-        console.log("No se han encontrado posts");
+    const postsStorage = localStorage.getItem('posts'); // Obtenemos los posts del localstorage
 
-      }
-    }));
+    // Si existen los posts ya guardados, los cargamos desde el json
+    if (postsStorage) {
+      this.posts = JSON.parse(postsStorage);
+    }
+    // Si no existen, los cargamos desde la API
+    else {
+      //Obtenemos los datos de la API de POSTS
+      this.socialPostApi.obtenerPosts().subscribe((post => {
+        // Si se encuentran posts en la API
+        if (Object.keys(post).length > 1) {
+          this.posts = post;
+          localStorage.setItem('posts', JSON.stringify(this.posts)); // Almacenamos localmente los posts en el dispositivo
+        }
+      }));
+    }
 
   }
 
